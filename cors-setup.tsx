ec746e55a07e3260c74395af47f796496147d306
Op-suvive <ruwaifi.hoque@gmail.com>
2025-07-13 04:04:32 -0400

    Add files via upload

---
+++ cors-setup.tsx
@@ -0,0 +1,7 @@
+function addCorsHeaders(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "\*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+}
+
+module.exports = addCorsHeaders;
